c83e98c3b35bfaf1c3788c0d2fca437c0b23c83d
sgtio <sejoruiz@gmail.com>
2018-07-02 08:32:55 +0200

    scripts: Cleanup lock.sh
    General cleanup and remove bashisms from lock.sh. Also, stopping spotify
    is now handled with a gdbus call, instead of a playerctl call, which
    means less dependencies

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,11 +1,17 @@
-#!/bin/bash
-icon="$HOME/.config/scripts/lock.png"
-tmpbg="$HOME/tmp/xscreensaver/locked.png"
+#!/bin/sh
 
-(( $# )) && { icon=$1; }
+icon=$HOME/.config/scripts/lock.png
+tmpbg=$HOME/tmp/xscreensaver/locked.png
 
-playerctl stop
-scrot "$tmpbg"
-convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
-convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
-i3lock -i $tmpbg 
+[ $# -eq 0 ] || icon=$1
+
+# Stop spotify if running
+gdbus call --session \
+	--dest org.mpris.MediaPlayer2.spotify \
+	--object-path /org/mpris/MediaPlayer2 \
+	--method org.mpris.MediaPlayer2.Player.Stop 2>&1 >/dev/null
+
+scrot $tmpbg
+convert $tmpbg -scale 10% -scale 1000% $tmpbg
+convert $tmpbg $icon -gravity center -composite -matte $tmpbg
+i3lock -i $tmpbg
