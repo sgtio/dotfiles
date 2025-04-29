19618b8fe998f0b7580ccf328e036b751854aca7
Sergio Ruiz <sejoruiz@gmail.com>
2025-04-29 10:50:05 +0200

    vim: Add rustaceanvim plugin
    This is good for Rust dev

--- vim/luafiles/init.lua
+++ vim/luafiles/init.lua
@@ -48,6 +48,11 @@ plugins = {
     },
     {
     'rust-lang/rust.vim'
+    },
+    {
+        'mrcjkb/rustaceanvim',
+        version = '^6', -- Recommended
+        lazy = false, -- This plugin is already lazy
     }
 }
 
