fb3f3d66f7c48b7b3320eb37e637e4ff1b71deae
sgtio <sejoruiz@gmail.com>
2025-05-14 11:46:06 +0200

    Move init.lua to main vim directory
    This allows us to simply symlink the entire directory

--- vim/init.lua
+++ vim/init.lua
@@ -1,15 +1,15 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 -- Auto bootstrapping of lazy.nvim disabled. Don't download automatically.
---if not vim.loop.fs_stat(lazypath) then
---  vim.fn.system({
---    "git",
---    "clone",
---    "--filter=blob:none",
---    "https://github.com/folke/lazy.nvim.git",
---    "--branch=stable", -- latest stable release
---    lazypath,
---  })
---end
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
 vim.opt.rtp:prepend(lazypath)
 
 plugins = {
@@ -59,5 +59,5 @@ plugins = {
 require("lazy").setup(plugins)
 
 -- Now that the plugins are loaded, source our vimrc file
-local vimrc = vim.fn.stdpath("config") .. "/vimrc.vim"
+local vimrc = vim.fn.stdpath("config") .. "/vimrc"
 vim.cmd.source(vimrc)
